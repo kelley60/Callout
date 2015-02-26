b0501c83e9267de97ee5f375d77c14784c602f2f
Austin <awirth@purdue.edu>
2015-02-26 14:08:27 -0500

    Added the club class with some basic methods

---
+++ app/src/main/java/ui/Club.java
@@ -0,0 +1,53 @@
+package ui;
+
+/**
+ * Created by Austin on 2/26/2015.
+ */
+public class Club {
+
+    public String name;
+    public int members; //maybe not needed
+    public ArrayList cat; //categories
+    public String bio; //bio/description of the club
+    public Club(String n, int m, ArrayList categories, String b)
+    {
+        name = n;
+        members = m;
+        cat = categories;
+        bio = b;
+    }
+    public void addCat(String newCat)
+    {
+        //add a new category to this club
+        //to do: look into using addAll(), figure out how collections work
+        //this way it could add multiple categories at once
+        cat.add(newCat);
+    }
+    public void removeCat(String badCat)
+    {
+        //remove badCat from the club's categories
+        //to do: look into using removeAll(), as above figure out collections
+        remove(badCat);
+    }
+    public void changeName(String newName)
+    {
+        //change the club's name
+        name = newName;
+    }
+    public void changeMembers(int numMembers)
+    {
+        //change number of members. maybe not needed
+        members = numMembers;
+    }
+    public void addMem()
+    {
+        //add just one member. maybe not needed
+        members++;
+    }
+    public void editBio(String newBio)
+    {
+        //edit he bio of the club. Maybe find a way to open a textbox to edit the previous bio
+        //instead of just assigning a new string to it
+        bio = newBio;
+    }
+}
