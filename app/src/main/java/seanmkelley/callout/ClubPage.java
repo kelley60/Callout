4a6b2980bc7eb5e05f25d8ebc49a0eb87a0b27f0
sirtrout <dylanbrashear@sbcglobal.net>
2015-03-04 22:35:59 -0500

    Added activity_student_club_view, for viewing a club from a student's perspective.

--- app/src/main/java/seanmkelley/callout/ClubPage.java
+++ app/src/main/java/seanmkelley/callout/ClubPage.java
@@ -11,7 +11,7 @@ public class ClubPage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_club_page);
+        setContentView(R.layout.activity_student_club_view);
     }
 
 
