3463f36a4ed981f6633acb9c20f0542b1578d590
wenright <wenright@purdue.edu>
2015-04-08 19:26:35 -0400

    Bug fixes and cleanup for HTTPGet and ClubMasterList

--- app/src/main/java/seanmkelley/callout/ClubMasterList.java
+++ app/src/main/java/seanmkelley/callout/ClubMasterList.java
@@ -3,19 +3,16 @@ package seanmkelley.callout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClubMasterList extends Activity {
-    public static final String TAG = ClubMasterList.class.getSimpleName();
     private List<Club> clubList;
     private ClubArrayAdapter adapter;
 
@@ -24,38 +21,8 @@ public class ClubMasterList extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_master_list);
-        //the following was in some example code I found online
-        //not quite sure what it does but it's probably important, seems to be error trapping
-         /*try {
-         Class.forName("org.apache.derby.jdbc.ClientDriver"); //driver? not sure what's supposed to go here
-        }
-        catch(ClassNotFoundException e) {
-         System.out.println("Class not found "+ e);
-         }
-        // TODO Get String list of club names, descriptions, and photos from database
-        clubNames = new ArrayList<String>(20);
-        //commenting all of my revisions for now so I don't break anything right before the demo
-        //clubBios = new ArrayList<String>(20);
-        try {
-            Connection con = DriverManager.getConnection("jdbc:mydb.ics.purdue.edu","awirth","alaina007");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM clubs");
-            //System.out.println("id  Name    Bio");
 
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("Name");
-                String bio = rs.getString("Bio");
-                //System.out.println(id+"   "+Name+"    "+Bio);
-                clubNames.add("Name");
-                //clubBios.add("Bio");
-            }
-        }
-        catch(SQLException e){
-            System.out.println("SQL exception occurred" + e);
-        }*/
-
-        List<Club> clubList = new ArrayList<Club>();
+        clubList = new ArrayList<Club>();
         ListView lv = (ListView) findViewById(R.id.masterClubListView);
         adapter = new ClubArrayAdapter(this, R.layout.list_item, clubList);
         lv.setAdapter(adapter);
@@ -70,40 +37,6 @@ public class ClubMasterList extends Activity {
         });
 
         HTTPGet.getClubList("http://web.ics.purdue.edu/~awirth/db_clubs.php", clubList, this);
-
-
-
-        /*
-        DbMailer db = new DbMailer("http://web.ics.purdue.edu/~awirth/db_clubs.php", getApplicationContext());
-
-        List<Club> clubNames = db.ClubList();
-        clubNames = new ArrayList<Club>();
-        */
-
-        //Toast.makeText(this, clubNames.get(0).getName(), Toast.LENGTH_LONG).show();
-        //for (int i = 0; i < clubNames.size(); i++){
-
-        //}
-
-
-        // Set to display club master list xml page
-
-
-
-        // Setup the list view to display the club names
-        /*
-        ListView lv = (ListView) findViewById(R.id.masterClubListView);
-        lv.setAdapter(new ClubArrayAdapter(this, R.layout.list_item, clubNames));
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View v, int position, long arg3) {
-                Intent i = new Intent("ClubPage");
-                i.putExtra("club_id", getItemIdAtIndex(position));
-                i.putExtra("club_name", getClubName(position));
-                startActivity(i);
-            }
-        });
-        */
     }
 
     public void updateClubList() {
