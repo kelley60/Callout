37eb91ef0d5aa5fa791159fa0eab938507071d43
sirtrout <dylanbrashear@sbcglobal.net>
2015-04-09 19:31:10 -0400

    Fixed errors I created and hadn't noticed

--- app/src/main/java/ui/StudentActivity.java
+++ app/src/main/java/ui/StudentActivity.java
@@ -21,23 +21,6 @@ public class StudentActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_home);
-
-        //get user data
-        Context context = StudentActivity.this;
-        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.callout_file_key), Context.MODE_PRIVATE);
-        //does this user have an id?
-        int userid = sharedPref.getInt(getString(R.string.userid), 0);
-        if(userid == 0) //does not exist
-        {
-            //we'll need to assign a new one from a (counter?) in the database, and set up the database to assign the next user's id
-            SharedPreferences.Editor ed = sharedPref.edit();
-            ed.putInt(getString(R.string.userid), 1);
-            ed.commit();
-        }
-        else //exists
-        {
-            //Great!
-        }
     }
 
     @Override
