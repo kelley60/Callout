47126d2d9347a737ed75c745955e681d199a2cf1
sirtrout <dylanbrashear@sbcglobal.net>
2015-04-28 01:28:06 -0400

    Update Favorites.java

--- app/src/main/java/seanmkelley/callout/Favorites.java
+++ app/src/main/java/seanmkelley/callout/Favorites.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,46 +17,21 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class Favorites extends Activity{
+public class Favorites extends ClubMasterList{
 
     private ArrayList<Club> masterClubList;
-    private ArrayList<Club> favoritesList;
+    private ArrayList<Club> clubList;
     private ClubArrayAdapter adapter;
-    private Button mBackButton;
-
+    private String userFavorites;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_favorites);
-        favoritesList = new ArrayList<Club>();
-
-        mBackButton = (Button) findViewById(R.id.favoritesBackButtonId);
-
-        mBackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-
-        ListView lv = (ListView) findViewById(R.id.listView2);
-        adapter = new ClubArrayAdapter(this, R.layout.list_item, favoritesList);
-        lv.setAdapter(adapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View v, int position, long arg3) {
-                Intent i = new Intent("ClubPage");
-                i.putExtra("club_name", getClubName(position));
-                i.putExtra("club_bio", getClubBio(position));
-                startActivity(i);
-            }
-        });
-
+        clubList = new ArrayList<Club>();
         //Retrieve favorites list, separate into array
         Context context = Favorites.this;
         SharedPreferences sp = context.getSharedPreferences(getString(R.string.callout_file_key), Context.MODE_PRIVATE);
-        String userFavorites = sp.getString(getString(R.string.user_favorites), "");
+        userFavorites = sp.getString(getString(R.string.user_favorites), "");
 
         //do they even have favorites?
         if(userFavorites.equals(""))
@@ -65,23 +39,36 @@ public class Favorites extends Activity{
             TextView t = (TextView) findViewById(R.id.errorView);
             t.setText("No Favorites Found");
         }
-       else
-        {
-            TextView t = (TextView) findViewById(R.id.errorView);
-            t.setText(userFavorites);
-            /*String[] favoritesNames = new String[userFavorites.length() / 2];
-            int c = 0;
-            int i = 0;
-            while (c < userFavorites.length())
-            {
-                favoritesNames[i].concat(userFavorites.substring(c, userFavorites.indexOf('\n', c) - 1));
-                c += favoritesNames[i].length() + 1;
-                i++;
-            }*/
+        else {
+            ListView lv = (ListView) findViewById(R.id.listView2);
+            adapter = new ClubArrayAdapter(this, R.layout.list_item, clubList);
+            lv.setAdapter(adapter);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapter, View v, int position, long arg3) {
+                    Intent i = new Intent("ClubPage");
+                    i.putExtra("club_name", getClubName(position));
+                    i.putExtra("club_bio", getClubBio(position));
+                    startActivity(i);
+                }
+            });
+
+            HTTPGet.getClubList(clubList, this);
         }
     }
 
+    public void setMasterClubList() {
+        masterClubList = new ArrayList<Club>(clubList);
+    }
 
+    public void updateClubList() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
 //    public void populate(){
 //        DbMailer db = new DbMailer("http://web.ics.purdue.edu/~awirth/db_clubs.php", getApplicationContext());
 //        List<Club> clubNames = db.ClubList();
@@ -107,6 +94,17 @@ public class Favorites extends Activity{
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_favorites, menu);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = clubList.size() - 1; i >= 0; i--)
+            if (!userFavorites.contains(clubList.get(i).getName()))
+                clubList.remove(i);
+        updateClubList();
         return false;
     }
 
@@ -126,10 +124,10 @@ public class Favorites extends Activity{
     }
 
     private String getClubName(int position) {
-        return favoritesList.get(position).getName();
+        return clubList.get(position).getName();
     }
 
     private String getClubBio(int position) {
-        return favoritesList.get(position).getBio();
+        return clubList.get(position).getBio();
     }
 }
